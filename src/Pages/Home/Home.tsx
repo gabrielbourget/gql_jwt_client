b7de544457ce435ff63c99f049445092e68c5e7a
Gabriel Bourget <gaboxa@syntronic.com>
2021-02-05 15:02:15 -0500

    refactor

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -14,7 +14,6 @@ import { setAccessToken } from '../../accessToken';
 import { useUsersQuery, useLogoutMutation } from "../../generated/graphql";
 // -> Within Component
 import { styleGen } from './HomeStyles';
-import { clearCookies } from '../../helpers';
 
 const Home: React.FC = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
